f0e3940876387aa427a8ee3016aecaf178b06223
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-07-19 02:00:52 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -11,7 +11,7 @@ const MainLogo = () => {
       <img 
         src="https://i.imgur.com/0PEVpuA.png" 
         alt="Amazon Logo" 
-        className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
+        className="h-18 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
     </div>
   );
